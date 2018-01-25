d40542fea9f2f8de46005cdc9849c9089c0713e2
kohlten <32156797+kohlten@users.noreply.github.com>
2018-01-25 06:33:22 -0700

    Added first files
    Added the heart

---
+++ config_day12.py
@@ -0,0 +1,113 @@
+import subprocess
+import os
+import sys
+
+#--------------------------ex01--------------------------
+os.chdir("work/ex00")
+text = "HELLO WORLD!\n"
+file = open("test.txt", "w")
+file.write(text);
+file.close()
+
+#Make
+pipe = subprocess.Popen(["make"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if "ft_display_file" not in os.listdir(os.getcwd()):
+	print(output)
+	print("DUM DUM DUM DUM: Make FAILED. File ft_display_file not found. " + ' '.join(os.listdir(os.getcwd())))
+	sys.exit(1)
+
+#Display output text of files
+pipe = subprocess.Popen(["./ft_display_file"], stdout=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if output != "File name missing.\n":
+	print("I GOT: " + output.replace("\n", "\\n"))
+	print("WHAT I EXPECTED: File name missing\\n")
+	print("DUM DUM DUM DUM!")
+	sys.exit(1);
+
+pipe = subprocess.Popen(["./ft_display_file", "T", "T"], stdout=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if output != "Too many arguments.\n":
+	print("I GOT: " + output.replace("\n", "\\n"))
+	print("WHAT I EXPECTED: Too many arguments.\\n")
+	print("DUM DUM DUM DUM!")
+	sys.exit(1);
+
+pipe = subprocess.Popen(["./ft_display_file", "Makefile"], stdout=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if output != "*contenu du Makefile*\n":
+	print("I GOT: " + output.replace("\n", "\\n"))
+	print("WHAT I EXPECTED: *contenu du Makefile*\\n")
+	print("DUM DUM DUM DUM!")
+	sys.exit(1);
+
+#Actually test shit
+pipe = subprocess.Popen(["./ft_display_file", "test.txt"], stdout=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if output[0:len(output)-1] != text:
+	print("I GOT: " + output.replace("\n", "\\n"))
+	print("WHAT I EXPECTED: " + text.replace("\n", "\\n"))
+	print("DUM DUM DUM DUM!")
+	sys.exit(1);
+
+#Clean up
+pipe = subprocess.Popen(["make", "clean"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if "ft_display_file" in os.listdir(os.getcwd()):
+	print(output)
+	print("DUM DUM DUM DUM: Make FAILED. ft_display_file still found! " + ' '.join(os.listdir(os.getcwd())))
+	sys.exit(1)
+
+print("ex00 GOOD!!! One smart for you!")
+#--------------------------ex01--------------------------
+os.chdir("../ex01")
+file = open("test.txt", "w")
+file.write(text);
+file.close()
+
+#Make
+pipe = subprocess.Popen(["make"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if "ft_cat" not in os.listdir("."):
+	print(output)
+	print("DUM DUM DUM DUM: Make FAILED. File ft_cat not found. " + ' '.join(os.listdir(".")))
+	sys.exit(1)
+
+#Test opening files
+pipe = subprocess.Popen(["./ft_cat", "test.txt"], stdout=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if output[0:len(output)-1] != text:
+	print("I GOT: " + output.replace("\n", "\\n"))
+	print("WHAT I EXPECTED: " + text.replace("\n", "\\n"))
+	print("DUM DUM DUM DUM!")
+	sys.exit(1);
+
+#Test reading from stdin
+pipe = subprocess.Popen(["echo", text, "|", "./ft_cat"], stdout=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if output[0:len(output)-1] != text:
+	print("I GOT: " + output.replace("\n", "\\n"))
+	print("WHAT I EXPECTED: " + text.replace("\n", "\\n"))
+	print("DUM DUM DUM DUM!")
+	sys.exit(1);
+
+#Clean up
+pipe = subprocess.Popen(["make", "clean"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output = pipe.communicate()[0]
+
+if "ft_display_file" in os.listdir(os.getcwd()):
+	print(output)
+	print("DUM DUM DUM DUM: Make FAILED. ft_display_file still found! " + ' '.join(os.listdir(os.getcwd())))
+	sys.exit(1)
+
+print("ex01 GOOD!!! One smart for you!")
---
+++ spawn.py
@@ -0,0 +1,39 @@
+import os, sys, subprocess
+
+for loc in os.listdir("."):
+	if "day" in loc:
+		location = loc
+		break
+folders = os.listdir(location)
+folders_to_be_copied = []
+files_to_be_copied = []
+
+if "ex00" in os.listdir("./work"):
+	f = os.listdir("work")
+	for i in range(len(f)):
+		subprocess.call(["rm", "-R", "work/" + f[i]])
+
+for i in range(len(folders)):
+	if folders[i][0:2] == "ex":
+		folders_to_be_copied.append(folders[i])
+		files = os.listdir(location + '/' + folders[i])
+
+		for j in range(len(files)):
+			files_to_be_copied.append(folders[i] + '/' + files[j])
+
+for i in range(len(folders_to_be_copied)):
+	os.mkdir("work/" + folders_to_be_copied[i])
+
+files_data = []
+for i in range(len(files_to_be_copied)):
+	file = open(location + '/' + files_to_be_copied[i], 'r')
+	files_data.append(file.read())
+	file.close()
+
+for i in range(len(files_data)):
+	file = open("work/" + files_to_be_copied[i], "w")
+	file.write(files_data[i])
+	file.close()
+	file = "work/" + files_to_be_copied[i][:len(files_to_be_copied[i]) - 1]
+
+subprocess.call(["python", "config_day12.py"])
